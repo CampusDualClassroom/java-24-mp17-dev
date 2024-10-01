3989eb35f5119d1b3996cc420ae80013aee7c52e
Julio Díaz <juliodiaz1792@gmail.com>
2024-10-01 10:25:31 +0200

    Solved exercise

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -1,18 +1,30 @@
 package com.campusdual.classroom;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
-
+        Queue<String> stringQueue = new LinkedList<>();
+        stringQueue.add("Smith");
+        stringQueue.add("Montessori");
+        stringQueue.add("Peralta");
+        stringQueue.add("House");
+        return stringQueue;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        Iterator<String> stringIterator = queue.iterator();
+        while (stringIterator.hasNext()) {
+            String elem = stringIterator.next();
+            stringIterator.remove();
+            System.out.println(elem);
+        }
     }
 
     public static void main(String[] args) {
-
+        printAndEmptyQueue(new LinkedList<>(List.of("1", "2", "3", "4")));
     }
 
 }
